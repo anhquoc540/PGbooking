e1a1d24520667d79c1cfc8f707f8c04dc657be1c
Huy Phung <huypc2410@gmail.com>
2022-02-23 13:14:31 +0700

    Add settings fragment

--- app/src/main/java/com/example/photographerbooking/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/photographerbooking/fragment/SettingsFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.photographerbooking.R;
 
@@ -19,12 +22,12 @@ public class SettingsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+    Button btnChangeProfile, btnChangeEmail;
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -41,26 +44,43 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
         return fragment;
+    }
+
+    private void binding(View view) {
+        btnChangeProfile = view.findViewById(R.id.btnChangeProfile);
+        btnChangeEmail = view.findViewById(R.id.btnChangeEmail);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Log.i("SettingsFragment", "onCreateView invoked");
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        binding(view);
+        btnChangeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "You clicked to change profile", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
+
+
 }
